7a9861627be2e73b3f3bbf3182b6306a9e00898f
Aliya Safina <ktotamy@gmail.com>
2020-02-24 18:13:38 +0300

    init commit

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-spin-nklafgulx1">
+            <div class="ldio-d3oou7moqo">
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
